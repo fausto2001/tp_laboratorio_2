3ab10c69e0d8d668e2fc006c1557ce2f9acf1df4
fausto <60607777+fausto2001@users.noreply.github.com>
2021-09-19 14:41:32 -0300

    Commit TP1

---
+++ tp_laboratorio_2/TP1/Entidades/Calculadora.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+        public static double Operar(Operando num1, Operando num2, char operador)
+        {
+            double ret = 0;
+            ValidarOperador(operador);
+
+            switch (operador)
+            {
+                case '+':
+                    ret = num1 + num2;
+                    break;
+                case '-':
+                    ret = num1 - num2;
+                    break;
+                case '*':
+                    ret = num1 * num2;
+                    break;
+                case '/':
+                    ret = num1 / num2;
+                    break;
+            }
+            return ret;
+        }
+
+        private static char ValidarOperador(char operador)
+        {
+            if (operador != '-' && operador != '*' && operador != '/')
+            {
+                operador = '+';
+            }
+
+            return operador;
+        }
+
+    }
+}
---
+++ tp_laboratorio_2/TP1/Entidades/Operando.cs
@@ -0,0 +1,149 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        private double numero;
+
+        public Operando()
+        {
+            numero = 0;
+        }
+
+        public Operando(double numero)
+        {
+            this.numero = numero;
+        }
+
+        public Operando(string strNumero)
+        {
+            this.Numero = strNumero;
+        }
+
+        private double ValidarOperando(string strNumero)
+        {
+            if (!double.TryParse(strNumero, out double ret))
+            {
+                ret = 0;
+            }
+            return ret;
+        }
+
+        private string Numero
+        {
+            set { this.numero = ValidarOperando(value); }
+        }
+
+        private static bool EsBinario(string binario)
+        {
+            int length = binario.Length;
+            int i = 0;
+            bool ret = true;
+
+            while (i < length && !ret)
+            {
+                if (binario[i] == '0' || binario[i] == '1')
+                {
+                    i++;
+                }
+                else
+                {
+                    ret = false;
+                }
+            }
+
+            return ret;
+        }
+
+        public static string BinarioDecimal(string binario)
+        {
+            string ret = "";
+
+            if (EsBinario(binario))
+            {
+                try
+                {
+                    ret = Convert.ToInt64(binario, 2).ToString();
+                }
+                catch(System.FormatException)
+                {
+                    ret = "Valor inválido";
+                }
+                catch(System.ArgumentException)
+                {
+                    ret = "No se admiten negativos";
+                }
+
+
+
+            }
+            else
+            {
+                ret = "Valor inválido";
+            }
+
+            return ret;
+        }
+
+        public static string DecimalBinario(double numero)
+        {
+            string ret = "Valor inválido";
+
+            if (numero >= 0)
+            {
+                int n = Convert.ToInt32(Math.Abs(numero));
+
+                ret = Convert.ToString(n, toBase: 2);
+            }
+
+            return ret;
+        }
+
+        public static string DecimalBinario(string numero)
+        {
+            string ret = "Valor inválido";
+            int aux = Convert.ToInt32(numero);
+
+            if (aux >= 0)
+            {
+                int n = Math.Abs(aux);
+                ret = Convert.ToString(n, toBase: 2);
+            }
+
+            return ret;
+        }
+
+        public static double operator -(Operando n1, Operando n2)
+        {
+            return n1.numero - n2.numero;
+        }
+
+        public static double operator +(Operando n1, Operando n2)
+        {
+            return n1.numero + n2.numero;
+        }
+
+        public static double operator *(Operando n1, Operando n2)
+        {
+            return n1.numero * n2.numero;
+        }
+
+        public static double operator /(Operando n1, Operando n2)
+        {
+            double ret;
+
+            if (n2.numero == 0)
+            {
+                ret = double.MinValue;
+            }
+            else
+            {
+                ret = n1.numero / n2.numero;
+            }
+
+            return ret;
+        }
+
+    }
+}
