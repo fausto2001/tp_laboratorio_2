369f036a5fc83648d70438bb991057caee877799
Fausto Panello <faustopanello2017@gmail.com>
2021-09-19 17:34:56 -0300

    Final Commit TP1

---
+++ TP1/Entidades/Calculadora.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entidades
+{
+    public class Calculadora
+    {
+        /// <summary>
+        /// Valida operador, y luego hace la cuenta que debe de acuerdo al operador obtenido.
+        /// </summary>
+        /// <param name="num1"></param>
+        /// <param name="num2"></param>
+        /// <param name="operador"></param>
+        /// <returns></returns>
+        public static double Operar(Operando num1, Operando num2, char operador)
+        {
+            double ret = 0;
+            ValidarOperador(operador);
+
+            switch (operador)
+            {
+                case '+':
+                    ret = num1 + num2;
+                    break;
+                case '-':
+                    ret = num1 - num2;
+                    break;
+                case '*':
+                    ret = num1 * num2;
+                    break;
+                case '/':
+                    ret = num1 / num2;
+                    break;
+            }
+            return ret;
+        }
+
+        /// <summary>
+        /// Valida operador. Si no es ninguno correcto, utiliza '+'.
+        /// </summary>
+        /// <param name="operador"></param>
+        /// <returns></returns>
+        private static char ValidarOperador(char operador)
+        {
+            if (operador != '-' && operador != '*' && operador != '/')
+            {
+                operador = '+';
+            }
+
+            return operador;
+        }
+
+    }
+}
